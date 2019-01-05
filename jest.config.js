56f33344fd25ee76f8d15ac747526971d1d69ab4
johnwatkins0 <johnwatkins0@gmail.com>
2019-01-05 15:02:23 -0600

    Move mocks folder

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
   testURL: 'http://localhost/',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__tests__/file-mock.ts',
+      '<rootDir>/src/__mocks__/file-mock.ts',
   },
 };
