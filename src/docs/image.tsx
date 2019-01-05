0745a3724a01dbb70da0ad8c3323e873a1660e65
johnwatkins0 <johnwatkins0@gmail.com>
2019-01-05 14:47:57 -0600

    Move docs src into src directory

---
+++ src/docs/image.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import DraggableSplitscreen from '../draggable-splitscreen';
+import leftSideImage from '../assets/left-side.png';
+import rightSideImage from '../assets/right-side.png';
+
+function LeftSide() {
+  return <img src={leftSideImage} alt="Left Side" style={{ width: '100%', height: 'auto' }} />;
+}
+
+function RightSide() {
+  return <img src={rightSideImage} alt="Right Side" style={{ width: '100%', height: 'auto' }} />;
+}
+
+export default function Image() {
+  return <DraggableSplitscreen leftSide={<LeftSide />} rightSide={<RightSide />} />;
+}
